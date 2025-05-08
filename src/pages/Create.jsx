f27527cd4cadf8684349144f10fe00cb80d3d9dc
BGC <uthmanajanaku@gmail.com>
2025-05-08 12:48:18 +0100

    goal server front end

---
+++ src/pages/Create.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import step from "../assets/amico.png";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useNavigate } from "react-router-dom";
+import { axiosInstance } from "../axiosInstance";
+
+const Create = () => {
+  const redirect = useNavigate();
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const handleCreateGoal = async (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+    try {
+      const { data } = await axiosInstance.post("/", { title, description });
+      if (data.success) {
+        redirect("/all");
+      }
+
+      //response //response.status === 201
+    } catch (error) {
+      toast.error("Title already exist");
+      setTitle("");
+      setDescription("");
+      setIsSubmitting(false);
+    }
+  };
+
+  return (
+    <div className="container d-flex justify-content-between align-items-center mt-3 pb-3 gap-lg-2">
+      <div className="main-form py-5 px-1 ps-lg-2 ps-xl-3 pe-xl-3 rounded-2">
+        <ToastContainer />
+        <form className="create-form" onSubmit={handleCreateGoal}>
+          <div className="mt-2">
+            <input
+              type="text"
+              placeholder="Goal Title"
+              className="bg-transparent"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              required
+            />
+          </div>
+          <div className="mt-5">
+            <textarea
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              name=""
+              id=""
+              cols="30"
+              rows="10"
+              placeholder="Goal Description"
+              className="bg-transparent"
+              required
+            ></textarea>
+          </div>
+          <div className="mt-2">
+            <button className="blue-bg p-2" disabled={isSubmitting}>
+              {isSubmitting ? "Creating Goal...." : "Create Goal"}
+            </button>
+          </div>
+        </form>
+      </div>
+      <div className="d-none d-lg-block main-img">
+        <img src={step} alt="image of a step" />
+      </div>
+    </div>
+  );
+};
+
+export default Create;
